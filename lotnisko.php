174f0f88a7a348f5349150289502782d4da2713f
PawelTyburski <biuro@tyburskistudio.pl>
2021-11-15 12:40:07 +0100

    Add files via upload

---
+++ lotnisko.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Port lotniczy</title>
+    <link rel="stylesheet" href="styl5.css">
+</head>
+<body>
+    <header>
+        <img src="zad5.png" alt="logo lotnisko">
+        <h1>Przyloty</h1>
+        <div>
+            <h3>Przydatne linki</h3>
+            <a target="_blank" href="kwerendy.txt">Pobierz...</a>
+        </div>
+    </header>
+    <main>
+        <table>
+            <tr>
+                <td>czas</td>
+                <td>kierunek</td>
+                <td>numer rejsu</td>
+                <td>status</td>
+            </tr>
+        </table>
+    </main>
+    <footer>
+        <section>
+
+        </section>
+        <section>
+
+        </section>
+    </footer>
+</body>
+</html>
